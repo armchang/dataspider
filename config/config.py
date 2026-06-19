784ec7a6910b45f2fd3c8b71ab8a92a5c708ad17
Armand Galagnara <armchang@gmail.com>
2026-06-20 10:43:34 +1200

    Keep local project version

--- config/config.py
+++ config/config.py
@@ -7,5 +7,3 @@ DATABASE_URL = os.getenv(
     "postgresql://postgres:postgres@localhost:5432/dataspider",
 )
 
-TURSO_URL = 'libsql://dataspider-manskie.aws-ap-northeast-1.turso.io'
-TURSO_TOKEN = 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3NzExNDQzNzEsImlkIjoiM2NjZjc3YjEtZmFkNi00NzUzLWJjZjQtOGJmZjI3Nzc4M2MxIiwicmlkIjoiY2FkOWY5NjMtNzc0OC00ODJhLTgwODgtMTJjOTk2MjljMWJkIn0.QiEOCcTcgq6u6iieoIZYFaj_78ivosMzLDkaITF5U5WNn_XCBV3eGSd1UIONfXsqHx9LNSEdGHW0TxfCI0VFBw'
